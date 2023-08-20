69d76b0ae89a1ad2f7350ec6ab0e39325c127b0b
Cédric <cedric.lavergn04@hotmail.fr>
2023-08-20 23:45:54 +0200

    Implémentation ajout et suppression de photo

--- assets/script.js
+++ assets/script.js
@@ -143,7 +143,7 @@ document.getElementById("login-item").addEventListener("click", (event) => {
 
 // Affichage modale
 
-const modalContainer = document.querySelector(".modal-container");
+const modalContainers = document.querySelectorAll(".modal-container-1");
 const modalTriggers = document.querySelectorAll(".modal-trigger");
 
 modalTriggers.forEach((trigger) =>
@@ -151,33 +151,195 @@ modalTriggers.forEach((trigger) =>
 );
 
 function toggleModal() {
-  modalContainer.classList.toggle("active");
+  modalContainers.forEach((container) => {
+    container.classList.toggle("active");
+  });
 }
 
-// Ajoute de la galerie dans la modale
+// Switch modal 1 à modal 2
+
+const modalContainers2 = document.querySelectorAll(".modal-container-2");
+const buttonAjoutPhoto = document.querySelector(".ajout-photo");
+
+buttonAjoutPhoto.addEventListener("click", toggleModal2);
+
+function toggleModal2() {
+  modalContainers.forEach((container) => {
+    container.classList.remove("active");
+  });
+  modalContainers2.forEach((container) => {
+    container.classList.toggle("active");
+  });
+}
+
+// Fermeture des modales
+
+const closeModal1 = document.querySelector("#modal-container-1 .close-modal");
+const closeModal2 = document.querySelector("#modal-container-2 .close-modal");
+const overlay1 = document.querySelector("#modal-container-1 .overlay");
+const overlay2 = document.querySelector("#modal-container-2 .overlay");
+
+closeModal1.addEventListener("click", () => {
+  const modal1 = document.getElementById("modal-container-1");
+  modal1.classList.remove("active");
+});
+
+closeModal2.addEventListener("click", () => {
+  const modal2 = document.getElementById("modal-container-2");
+  modal2.classList.remove("active");
+});
+
+overlay1.addEventListener("click", () => {
+  const modal1 = document.getElementById("modal-container-1");
+  modal1.classList.remove("active");
+});
+
+overlay2.addEventListener("click", () => {
+  const modal2 = document.getElementById("modal-container-2");
+  modal2.classList.remove("active");
+});
+
+// Ajout de la galerie dans la modale
 
 const btnModifier = document.querySelector(".modifier-button");
 
 btnModifier.addEventListener("click", () => {
-  galerieModale.innerHTML = allProjects
-    .map((projet) => {
-      return `<div class= "galerie-content">
-    <img src= "${projet.imageUrl}">
-    <span class= "delete-img"><i class="fa-solid fa-trash-can"></i></span>
-    <p class= "editer">éditer</p>
-    </div>`;
-    })
-    .join("");
-
-  const deleteButtons = document.querySelectorAll(".delete-img");
-  deleteButtons.forEach((button) =>
-    button.addEventListener("click", deleteImage)
-  );
+  displayGalerie();
 });
 
-function deleteImage(event) {
-  const imageId = event.target.getAttribute("id");
+function displayGalerie() {
+  galerieModale.innerHTML = allProjects
+    .map((projet) => {
+      return `<div class="galerie-content">
+        <img src="${projet.imageUrl}">
+        <span class="delete-img"  data-id="${projet.id}">
+        <i class="fa-solid fa-trash-can" data-id="${projet.id}"></i>
+        </span>
+        <p class="editer">éditer</p>
+      </div>`;
+    })
+    .join("");
+  const deleteButtons = document.querySelectorAll(".delete-img");
+  deleteButtons.forEach((button) =>
+    button.addEventListener("click", (event) => {
+      deleteImage(event);
+    })
+  );
 }
+
+async function deleteImage(event) {
+  const idPictures = parseInt(event.target.dataset.id);
+  console.log(idPictures);
+  try {
+    await fetch(`http://localhost:5678/api/works/${idPictures}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Berear ${localStorage.getItem("authToken")}`,
+      },
+    });
+    allProjects = allProjects.filter((project) => {
+      return project.id != idPictures;
+    });
+    displayGalerie();
+    displayProjets(allProjects);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Filtre dynamique
+
+const categorySelect = document.querySelector("#category");
+
+async function loadCategories() {
+  try {
+    const response = await fetch("http://localhost:5678/api/categories");
+    const data = await response.json();
+
+    data.forEach((category) => {
+      const option = document.createElement("option");
+      option.value = category.id;
+      option.textContent = category.name;
+      categorySelect.appendChild(option);
+    });
+  } catch (error) {
+    console.error("Erreur lors du chargement des catégories:", error);
+  }
+}
+
+loadCategories();
+
+// Delete photo
+
+// Ce qu'il me faut :
+// Quand je clique sur l'icone poubelle d'une photo, il faut que la photo en question disparaisse sans toucher aux autres photos
 
 // Dataset permet de mettre des id (ou d'autres données) dans un élément html
 // Quand click sur poubelle = regardé le dataset de l'image cliqué
+
+// Ajout de photo
+
+const ajoutPhotoButton = document.querySelector(".button-ajout-photo");
+let previewImage;
+let file = null;
+const ajoutContent = document.querySelector(".ajout-content");
+const validationButton = document.querySelector(".validation");
+
+ajoutPhotoButton.addEventListener("click", () => {
+  const fileInput = document.createElement("input");
+  fileInput.type = "file";
+  fileInput.accept = "image/jpeg,image/png";
+  fileInput.addEventListener("change", handleFileSelect);
+  fileInput.click();
+});
+
+function handleFileSelect(event) {
+  file = event.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      const previewImage = document.createElement("img");
+      previewImage.src = e.target.result;
+      previewImage.classList.add("miniature-image");
+
+      const ajoutContent = document.querySelector(".ajout-content");
+      ajoutContent.innerHTML = "";
+      ajoutContent.appendChild(previewImage);
+    };
+    reader.readAsDataURL(file);
+  }
+}
+
+// Écouteur d'événement pour le clic sur le bouton "Valider"
+
+validationButton.addEventListener("click", async (event) => {
+  if (event.target.classList.contains("validation")) {
+    const titleInput = document.querySelector("#title");
+    const categorySelect = document.querySelector("#category");
+
+    const formData = new FormData();
+    formData.append("title", titleInput.value);
+    formData.append("image", file);
+    formData.append("category", parseInt(categorySelect.value));
+
+    try {
+      const response = await fetch("http://localhost:5678/api/works", {
+        method: "POST",
+        headers: {
+          Authorization: `Berear ${localStorage.getItem("authToken")}`,
+        },
+        body: formData,
+      });
+      const data = await response.json();
+      console.log(data);
+
+      console.log("Nouvelle photo ajoutée avec succès !");
+      allProjects.push(data);
+      displayGalerie();
+      displayProjets(allProjects);
+      document.querySelector("#modal-container-2").classList.remove("active");
+    } catch (error) {
+      console.error("Erreur lors de la requête d'ajout de photo :", error);
+    }
+  }
+});
